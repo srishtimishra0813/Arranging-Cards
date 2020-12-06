b3559fb59fdfcb0479011813956ff3b9929789cd
srishtimishra0813 <75198063+srishtimishra0813@users.noreply.github.com>
2020-12-06 16:05:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-  var i,j;
-        var list = document.getElementById("list");
+var i,j;
+var answer = document.getElementById("result");
 function swap(arr, index1, index2){
-    var temp = arr[index1];
-    arr[index1] = arr[index2];
-    arr[index2]= temp;
+  var temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
 }
+
 function swapCards(arr){
-for(i=0; i<arr.length-1; i++){
-    for (j=0; j<arr.length-1; j++){
-        if(arr[j]>arr[j+1]){
-            swap(arr, j, j+1)
-        }
+  for (i = 0; i < arr.length - 1; i++){
+    for (j = 0; j < arr.length - 1; j++){
+      if(arr[j] > arr[j+1]){
+        swap(arr, j , j+1);
+      }
     }
+  }
+  return arr;
 }
-return arr;
-}
-list.innerHTML=swapCards(["Home", 11 , 7 , 4 , 1 , 6 , 13 , 10 , 8 , 9]);
+answer.innerHTML=swapCards(["Home", 11 , 7 , 4 , 1 , 6 , 13 , 10 , 8 , 9]);
